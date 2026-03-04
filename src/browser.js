704b74615aa18961d1b0c4891432fe2617f3ae21
Ben Higham <ben@benhigham.com>
2026-03-04 16:24:48 +1100

    refactor: inline confusing browser globals list, remove abandoned dependency

--- src/browser.js
+++ src/browser.js
@@ -1,4 +1,3 @@
-import confusingBrowserGlobals from 'confusing-browser-globals';
 import globals from 'globals';
 
 import compatConfig from './plugins/compat.js';
@@ -6,6 +5,71 @@ import { domConfig as testingLibraryConfig } from './plugins/testing-library.js'
 import typescriptConfig from './typescript.js';
 
 /** @import { Linter } from 'eslint' */
+
+/**
+ * Browser globals that are confusing when used without `window.` prefix.
+ * @see {@link https://github.com/facebook/create-react-app/tree/main/packages/confusing-browser-globals}
+ */
+const confusingGlobals = [
+  'addEventListener',
+  'blur',
+  'close',
+  'closed',
+  'confirm',
+  'defaultStatus',
+  'defaultstatus',
+  'event',
+  'external',
+  'find',
+  'focus',
+  'frameElement',
+  'frames',
+  'history',
+  'innerHeight',
+  'innerWidth',
+  'length',
+  'location',
+  'locationbar',
+  'menubar',
+  'moveBy',
+  'moveTo',
+  'name',
+  'onblur',
+  'onerror',
+  'onfocus',
+  'onload',
+  'onresize',
+  'onunload',
+  'open',
+  'opener',
+  'opera',
+  'outerHeight',
+  'outerWidth',
+  'pageXOffset',
+  'pageYOffset',
+  'parent',
+  'print',
+  'removeEventListener',
+  'resizeBy',
+  'resizeTo',
+  'screen',
+  'screenLeft',
+  'screenTop',
+  'screenX',
+  'screenY',
+  'scroll',
+  'scrollbars',
+  'scrollBy',
+  'scrollTo',
+  'scrollX',
+  'scrollY',
+  'self',
+  'status',
+  'statusbar',
+  'stop',
+  'toolbar',
+  'top',
+];
 
 /** @type {Linter.Config[]} */
 export const baseConfig = [
@@ -18,7 +82,7 @@ export const baseConfig = [
       },
     },
     rules: {
-      'no-restricted-globals': ['error', ...confusingBrowserGlobals],
+      'no-restricted-globals': ['error', ...confusingGlobals],
     },
   },
 ];
